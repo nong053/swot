206aeaacc2e3db3f2afbec16ffc4f204b8722435
KOSIT AROMSAVA <kosit.arom@gmail.com>
2023-02-27 10:44:47 +0700

    commited

--- mc.php
+++ mc.php
@@ -801,7 +801,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.3.js" integrity="sha256-nQLuAZGRRcILA+6dMBOvcRh5Pe310sBpanc6+QBmyVM=" crossorigin="anonymous"></script>
     <script src="https://kendo.cdn.telerik.com/2022.3.1109/js/kendo.all.min.js"></script>
-    <script src="Controller/cm.js"></script>
+    <script src="Controller/mc.js"></script>
     <script>
         $(document).ready(function(){
             $("#btn-back").click(function(){
