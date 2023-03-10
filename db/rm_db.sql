8952a6c1227185f22dc28be8f3dac7d538b29754
KOSIT AROMSAVA <kosit.arom@gmail.com>
2023-03-10 15:27:00 +0700

    update rm database

--- db/rm_db.sql
+++ db/rm_db.sql
@@ -41,8 +41,36 @@ CREATE TABLE `impact_master` (
 
 LOCK TABLES `impact_master` WRITE;
 /*!40000 ALTER TABLE `impact_master` DISABLE KEYS */;
-INSERT INTO `impact_master` VALUES (1,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','1','ผลกระทบต่ำมาก','อธิบายโอกาสเกิด',11,NULL,'2023-03-07 14:20:33'),(2,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','2','ผลกระทบต่ำ','อธิบายโอกาสเกิด',21,NULL,'2023-03-07 14:20:33'),(3,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','3','ผลกระทบปานกลาง','อธิบายโอกาสเกิด',31,NULL,'2023-03-07 14:20:33'),(4,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','4','ผลกระทบสูง','อธิบายโอกาสเกิด',41,NULL,'2023-03-07 14:20:33'),(5,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','5','ผลกระทบสูงมาก','อธิบายโอกาสเกิด',51,NULL,'2023-03-07 14:20:33');
+INSERT INTO `impact_master` VALUES (1,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','1','ผลกระทบต่ำมาก','อธิบายโอกาสเกิด',1,NULL,'2023-03-09 10:15:09'),(2,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','2','ผลกระทบต่ำ','อธิบายโอกาสเกิด',2,NULL,'2023-03-09 10:15:09'),(3,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','3','ผลกระทบปานกลาง','อธิบายโอกาสเกิด',3,NULL,'2023-03-09 10:15:09'),(4,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','4','ผลกระทบสูง','อธิบายโอกาสเกิด',4,NULL,'2023-03-09 10:15:09'),(5,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','5','ผลกระทบสูงมาก','อธิบายโอกาสเกิด',5,NULL,'2023-03-09 10:15:09');
 /*!40000 ALTER TABLE `impact_master` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `impact_master_ex`
+--
+
+DROP TABLE IF EXISTS `impact_master_ex`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `impact_master_ex` (
+  `im_id` int(11) NOT NULL,
+  `rce_id` varchar(45) DEFAULT NULL,
+  `uu_id` varchar(100) DEFAULT NULL,
+  `im_score` int(11) DEFAULT NULL,
+  `im_name` varchar(100) DEFAULT NULL,
+  `im_description` varchar(255) DEFAULT NULL,
+  `im_code` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`im_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `impact_master_ex`
+--
+
+LOCK TABLES `impact_master_ex` WRITE;
+/*!40000 ALTER TABLE `impact_master_ex` DISABLE KEYS */;
+/*!40000 ALTER TABLE `impact_master_ex` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -71,8 +99,35 @@ CREATE TABLE `likelihood_master` (
 
 LOCK TABLES `likelihood_master` WRITE;
 /*!40000 ALTER TABLE `likelihood_master` DISABLE KEYS */;
-INSERT INTO `likelihood_master` VALUES (1,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','1','โอกาสเกิดต่ำมาก',12,'อธิบายโอกาสเกิดความเสี่ยง',NULL,'2023-03-07 14:16:36'),(2,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','2','โอกาสเกิดต่ำ',22,'อธิบายโอกาสเกิดความเสี่ยง',NULL,'2023-03-07 14:16:36'),(3,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','3','โอกาสเกิดปานกลาง',32,'อธิบายโอกาสเกิดความเสี่ยง',NULL,'2023-03-07 14:16:36'),(4,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','4','โอกาสเกิดสูง',42,'อธิบายโอกาสเกิดความเสี่ยง',NULL,'2023-03-07 14:16:36'),(5,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','5','โอกาสเกิดสูงมาก',52,'อธิบายโอกาสเกิดความเสี่ยง',NULL,'2023-03-07 14:16:36');
+INSERT INTO `likelihood_master` VALUES (1,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','1','โอกาสเกิดต่ำมาก',1,'อธิบายโอกาสเกิดความเสี่ยง',NULL,'2023-03-09 10:19:22'),(2,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','2','โอกาสเกิดต่ำ',2,'อธิบายโอกาสเกิดความเสี่ยง',NULL,'2023-03-09 10:19:22'),(3,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','3','โอกาสเกิดปานกลาง',3,'อธิบายโอกาสเกิดความเสี่ยง',NULL,'2023-03-09 10:19:22'),(4,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','4','โอกาสเกิดสูง',4,'อธิบายโอกาสเกิดความเสี่ยง',NULL,'2023-03-09 10:19:22'),(5,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','5','โอกาสเกิดสูงมาก',5,'อธิบายโอกาสเกิดความเสี่ยง',NULL,'2023-03-09 10:19:22');
 /*!40000 ALTER TABLE `likelihood_master` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `likelihood_master_ex`
+--
+
+DROP TABLE IF EXISTS `likelihood_master_ex`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `likelihood_master_ex` (
+  `lh_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rce_id` int(11) DEFAULT NULL,
+  `lh_score` int(11) DEFAULT NULL,
+  `lh_name` int(11) DEFAULT NULL,
+  `lh_description` varchar(255) DEFAULT NULL,
+  `lh_code` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`lh_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `likelihood_master_ex`
+--
+
+LOCK TABLES `likelihood_master_ex` WRITE;
+/*!40000 ALTER TABLE `likelihood_master_ex` DISABLE KEYS */;
+/*!40000 ALTER TABLE `likelihood_master_ex` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -110,8 +165,35 @@ CREATE TABLE `risk` (
 
 LOCK TABLES `risk` WRITE;
 /*!40000 ALTER TABLE `risk` DISABLE KEYS */;
-INSERT INTO `risk` VALUES (18,'RISK18','4b7e2fd0-776a-420d-bd09-79a58da47ff6',0,'117','227','337','447','447','77','447','1','1','1',55,'2023-03-07 09:44:07','2023-03-07 10:07:28'),(19,'RISK19','4b7e2fd0-776a-420d-bd09-79a58da47ff6',0,'55','55','55','55','55','55','55','1','1','1',55,'2023-03-07 10:07:14','2023-03-07 10:07:28');
+INSERT INTO `risk` VALUES (18,'RISK18','4b7e2fd0-776a-420d-bd09-79a58da47ff6',0,'117','227','337','447','447','77','447','1','5','1',5,'2023-03-07 09:44:07','2023-03-09 14:16:48'),(19,'RISK19','4b7e2fd0-776a-420d-bd09-79a58da47ff6',0,'55','55','55','55','55','55','55','2','3','1',6,'2023-03-07 10:07:14','2023-03-09 14:16:48');
 /*!40000 ALTER TABLE `risk` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `risk_cate_ex`
+--
+
+DROP TABLE IF EXISTS `risk_cate_ex`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `risk_cate_ex` (
+  `rce_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rce_name` varchar(100) DEFAULT NULL,
+  `rce_type_code` int(11) DEFAULT NULL,
+  `rce_type_name` varchar(100) DEFAULT NULL,
+  `uu_id` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`rce_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `risk_cate_ex`
+--
+
+LOCK TABLES `risk_cate_ex` WRITE;
+/*!40000 ALTER TABLE `risk_cate_ex` DISABLE KEYS */;
+INSERT INTO `risk_cate_ex` VALUES (1,'บริหารความเสี่ยว1',1,'ผู้ดูแลระบบ','4b7e2fd0-776a-420d-bd09-79a58da47ff6'),(2,'บริหารความเสี่ยว222qqqd',2,'สาธารณะ','4b7e2fd0-776a-420d-bd09-79a58da47ff6');
+/*!40000 ALTER TABLE `risk_cate_ex` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -141,8 +223,70 @@ CREATE TABLE `risk_evaluation_master` (
 
 LOCK TABLES `risk_evaluation_master` WRITE;
 /*!40000 ALTER TABLE `risk_evaluation_master` DISABLE KEYS */;
-INSERT INTO `risk_evaluation_master` VALUES (1,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','112','12','12','12',NULL,'2023-03-07 15:10:24','1'),(2,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','12','12','12','12',NULL,'2023-03-07 15:10:24','2'),(3,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','12','12','12','12',NULL,'2023-03-07 15:10:24','3'),(4,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','12','12','12','12',NULL,'2023-03-07 15:10:24','4');
+INSERT INTO `risk_evaluation_master` VALUES (1,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','ต่ำ','1','8','white',NULL,'2023-03-09 14:13:30','1'),(2,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','ปานกลาง','9','16','yellow',NULL,'2023-03-09 14:13:31','2'),(3,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','สูง','17','24','orange',NULL,'2023-03-09 14:13:31','3'),(4,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','สูงมาก','25','25','red',NULL,'2023-03-09 14:13:31','4');
 /*!40000 ALTER TABLE `risk_evaluation_master` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `risk_evaluation_master_ex`
+--
+
+DROP TABLE IF EXISTS `risk_evaluation_master_ex`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `risk_evaluation_master_ex` (
+  `re_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rce_id` int(11) DEFAULT NULL,
+  `r_score_start` int(11) DEFAULT NULL,
+  `r_score_end` int(11) DEFAULT NULL,
+  `r_score_color` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`re_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `risk_evaluation_master_ex`
+--
+
+LOCK TABLES `risk_evaluation_master_ex` WRITE;
+/*!40000 ALTER TABLE `risk_evaluation_master_ex` DISABLE KEYS */;
+/*!40000 ALTER TABLE `risk_evaluation_master_ex` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `risk_ex`
+--
+
+DROP TABLE IF EXISTS `risk_ex`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `risk_ex` (
+  `r_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rce_id` int(11) DEFAULT NULL,
+  `r_seq` int(11) DEFAULT NULL,
+  `r_name` varchar(255) DEFAULT NULL,
+  `r_description` varchar(255) DEFAULT NULL,
+  `r_factor` varchar(255) DEFAULT NULL,
+  `r_effect` varchar(255) DEFAULT NULL,
+  `r_code` varchar(45) DEFAULT NULL,
+  `responsible` varchar(200) DEFAULT NULL,
+  `guidelines_risk` varchar(200) DEFAULT NULL,
+  `duration_of_work` varchar(255) DEFAULT NULL,
+  `stm_code` varchar(100) DEFAULT NULL,
+  `im_code` varchar(100) DEFAULT NULL,
+  `lh_code` varchar(100) DEFAULT NULL,
+  `total_score` int(11) DEFAULT NULL,
+  PRIMARY KEY (`r_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `risk_ex`
+--
+
+LOCK TABLES `risk_ex` WRITE;
+/*!40000 ALTER TABLE `risk_ex` DISABLE KEYS */;
+/*!40000 ALTER TABLE `risk_ex` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -170,8 +314,34 @@ CREATE TABLE `strategy_type_master` (
 
 LOCK TABLES `strategy_type_master` WRITE;
 /*!40000 ALTER TABLE `strategy_type_master` DISABLE KEYS */;
-INSERT INTO `strategy_type_master` VALUES (1,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','1','กลยุทธ์ที่1','อธิบายของกลยุทธ์12',NULL,'2023-03-07 14:29:24'),(2,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','2','กลยุทธ์ที่2','อธิบายของกลยุทธ์12',NULL,'2023-03-07 14:29:24'),(3,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','3','กลยุทธ์ที่3','อธิบายของกลยุทธ์12',NULL,'2023-03-07 14:29:24'),(4,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','4','กลยุทธ์ที่4','อธิบายของกลยุทธ์12',NULL,'2023-03-07 14:29:24');
+INSERT INTO `strategy_type_master` VALUES (1,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','1','กลยุทธ์ที่1','อธิบายของกลยุทธ์1',NULL,'2023-03-09 13:35:10'),(2,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','2','กลยุทธ์ที่2','อธิบายของกลยุทธ์121',NULL,'2023-03-09 13:35:10'),(3,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','3','กลยุทธ์ที่3','อธิบายของกลยุทธ์121',NULL,'2023-03-09 13:35:10'),(4,'4b7e2fd0-776a-420d-bd09-79a58da47ff6','4','กลยุทธ์ที่4','อธิบายของกลยุทธ์121',NULL,'2023-03-09 13:35:10');
 /*!40000 ALTER TABLE `strategy_type_master` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `strategy_type_master_ex`
+--
+
+DROP TABLE IF EXISTS `strategy_type_master_ex`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `strategy_type_master_ex` (
+  `stm_id` int(11) NOT NULL AUTO_INCREMENT,
+  `rce_id` int(11) DEFAULT NULL,
+  `stm_name` varchar(100) DEFAULT NULL,
+  `stm_description` varchar(255) DEFAULT NULL,
+  `stm_code` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`stm_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `strategy_type_master_ex`
+--
+
+LOCK TABLES `strategy_type_master_ex` WRITE;
+/*!40000 ALTER TABLE `strategy_type_master_ex` DISABLE KEYS */;
+/*!40000 ALTER TABLE `strategy_type_master_ex` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -213,4 +383,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-07 15:11:53
+-- Dump completed on 2023-03-10 15:26:42
